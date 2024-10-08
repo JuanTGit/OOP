956d2fe5ea99079e7aecf4d9beb23170749e42ac
Juan <j.tejeda46@yahoo.com>
2024-10-08 05:50:58 -0500

    game inventory class

---
+++ game-inventory/main.py
@@ -0,0 +1,22 @@
+# Problem: Video Game Inventory System
+# You are tasked with designing a game inventory system for a role-playing game (RPG). 
+# Players can collect items such as weapons, armor, potions, and other equipment. 
+# The system should allow the player to manage their inventory efficiently.
+
+"""
+Classes:
+	Item:
+		name
+		weight
+		value
+
+		subclasses:
+			weapons:
+			armor:
+			potion:
+	
+	Inventory:
+		items
+
+
+"""
