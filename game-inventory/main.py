9836fad340226b00cb07671fe4cc846de2205919
Juan <j.tejeda46@yahoo.com>
2024-10-09 01:15:59 -0500

    added items

--- game-inventory/main.py
+++ game-inventory/main.py
@@ -10,13 +10,79 @@ Classes:
 		weight
 		value
 
+		methods:
+			use
+
 		subclasses:
 			weapons:
+				damage
+				durability
+
+				methods:
+					use
 			armor:
+				defense
+				durability
+
+				methods:
+					use
 			potion:
+				healing_amount
+
+				methods:
+					use
 	
 	Inventory:
 		items
+		max_weight
 
-
+		methods:
+			add_item
+			remove_items
+			get_total_weight
+			display_items
 """
+
+class Item:
+	def __init__(self, name, weight, value):
+		self.name = name
+		self.weight = weight
+		self.value = value
+
+	def use(self):
+		print(f'You have used {self.name}.')
+	
+class Weapon(Item):
+	def __init__(self, name, weight, value, damage, durability=10):
+		super().__init__(name, weight, value)
+		self.damage = damage
+		self.durability = durability
+
+	def use(self):
+		if self.durability > 0:
+			self.durability -= 1
+		elif self.durability == 0:
+			return f'{self.name} has broken'
+		
+class Armor(Item):
+	def __init__(self, name, weight, value, defense, durability=10):
+		super().__init__(name, weight, value)
+		self.defense = defense
+		self.durability = durability
+	
+	def use(self):
+		if self.durability > 0:
+			self.durability -= 1
+		elif self.durability == 0:
+			return f'{self.name} has broken'
+		
+
+class Potion(Item):
+	def __init__(self, name, weight, value, healing_amount=50):
+		super().__init__(name, weight, value)
+		self.healing_amount = healing_amount
+	
+	def use(self):
+		print(f'{self.name} has been used.')
+
+scimitar = Weapon()
