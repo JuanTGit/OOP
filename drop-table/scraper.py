bea3693890c16727f5232ae92e07f3c75d8ed0fe
Juan <j.tejeda46@yahoo.com>
2024-11-13 00:06:39 -0600

    added variable for multiple tables

--- drop-table/scraper.py
+++ drop-table/scraper.py
@@ -12,7 +12,7 @@ for table in bandos_tables:
 	table_name = table.find_previous('span', attrs={'class': 'mw-headline'}).text
 
 	rows = table.find_all('tr')
-	
+
 	for row in rows[1:]:
 		# Item Name Row
 		item_name_cell = row.find('td', attrs={'class': 'item-col'})
@@ -26,8 +26,13 @@ for table in bandos_tables:
 		if item_name_cell and item_quantity_cell and item_rarity_cell and item_value_cell:
 			item_name = item_name_cell.text.strip()
 			item_quantity = item_quantity_cell['data-sort-value']
-			item_rarity = item_rarity_cell['data-drop-fraction']
+			item_rarity = item_rarity_cell['data-drop-fraction'].replace(',', '')
 			item_value = item_value_cell['data-sort-value']
+
+			def convert_drops(str):
+				if '/' in str:
+					numerator, denominator = map(float, str.split('/'))
+					return (numerator / denominator)
 
 			if table_name not in item_details:
 				item_details[table_name] = []
@@ -36,7 +41,7 @@ for table in bandos_tables:
 			item_details[table_name].append({
 				'name': item_name,
 				'quantity': item_quantity,
-				'rarity': item_rarity,
+				'rarity': convert_drops(item_rarity),
 				'value': item_value
 			})
 
--- drop-table/table.py
+++ drop-table/table.py
@@ -6,58 +6,33 @@
 	rares
 """
 import random
-# from player import juant
 from player import Player
 from scraper import item_details
-from flask import Flask
 
-app = Flask(__name__)
+class DropTable:
 
-# print(item_details)
-class DropTable:    
-    drop_rates = {
-        'rares': 1500,
-        'weapons': 128,
-        'ammo': 64,
-        'herbs': 32,
-        'other': 4
-    }
-
-    def __init__(self):
-        self.table = {
-            'rares': {},      # 1 in 1500 drop rate
-            'weapons': {},   # 1 in 128 drop rate
-            'ammo': {},      # 1 in 64 drop rate
-            'herbs': {},    # 1 in 32 drop rate
-            'other': {}     # 1 in 4 drop rate
-        }
-
-    def add_item(self, category, item, quantity=1):
-        if category in self.table:
-            if item in self.table[category]:
-                self.table[category][item] += quantity
-            else:
-                self.table[category][item] = quantity
-        else:
-            print(f"Category '{category}' does not exist.")
-
-    def add_items(self, category, dictItems):
-        if category in self.table:
-            for k, v in dictItems.items():
-                self.table[category][k] = v
+    def __init__(self, table):
+        self.table = table
     
-    def get_drop(self, player):
-        for category, items in self.table.items():
-            rate = self.drop_rates[category]
-            if items and random.randint(1, rate) == 1 or category == 'other':
-                drop = random.choice(list(items.keys()))
-                drop_copy = items[drop].copy()
-                player.add_item({drop: drop_copy})
-                return (f"Raid's Completed: {player.raid_counter}", sorted(player.inventory.items(), key=lambda item: item[1]['value'], reverse=True), f'Total Value: {player.calculate_inventory():,} gp')
+    def get_drop(self):
+        
+        for table, items in self.table.items():
+            print(f"======================={table}=======================")
+
+            total_probability = sum(item['rarity'] for item in items)
+            print(total_probability)
+
+            # for item in items:
+            #     print(item['rarity'])
+            # for item in items:
+            #     item['rarity'] = int(item['rarity'])
             
 
-for table, items in item_details.items():
-    print(table)
+new_table = DropTable(item_details)
+
+new_table.get_drop()
+
+
 
 randomizer = random.random()
 rarity = 8/127
@@ -65,3 +40,7 @@ rarity = 8/127
 didDrop = randomizer < rarity
 
 # print(f"{didDrop}, randomizer: {randomizer} rarity: {rarity}")
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
