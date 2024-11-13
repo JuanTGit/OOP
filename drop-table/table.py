132f48424ba2e136590c7576693c1a924df369bf
Juan <j.tejeda46@yahoo.com>
2024-11-13 01:30:33 -0600

    completed drop table for any wiki mob

--- drop-table/table.py
+++ drop-table/table.py
@@ -13,15 +13,40 @@ class DropTable:
 
     def __init__(self, table):
         self.table = table
+        self.probability_table = {}
+        
+
+        for table, items in self.table.items():
+            cumulative_drop_rate = 0
+            for item in items:
+                cumulative_drop_rate += item['rarity']
+            self.probability_table[table] = cumulative_drop_rate
+
     
     def get_drop(self):
+        table_roll = random.random()
+        cumulative_table_prob = 0
+
+        selected_table = None
+        for table, chance in self.probability_table.items():
+            cumulative_table_prob += chance
+            if table_roll <= cumulative_table_prob:
+                selected_table = table
+                break
         
-        for table, items in self.table.items():
-            print(f"======================={table}=======================")
+        if selected_table is None:
+            selected_table = "Other"
 
-            total_probability = sum(item['rarity'] for item in items)
-            print(total_probability)
-
+        items = self.table[selected_table]
+        cumulative_rarity = 0
+        item_roll = random.random()
+        
+        for item in items:
+            cumulative_rarity += item['rarity']
+            if item_roll <= cumulative_rarity:
+                return item
+        
+        return random.choice(items)
             # for item in items:
             #     print(item['rarity'])
             # for item in items:
@@ -31,7 +56,6 @@ class DropTable:
 new_table = DropTable(item_details)
 
 new_table.get_drop()
-
 
 
 randomizer = random.random()
